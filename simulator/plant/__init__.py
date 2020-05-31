e9a3e2dad6f6267447b5b9cd4b97c51b36a8cf3a
Varun Iyer <varun_iyer@posteo.net>
2020-05-30 21:45:32 -0700

    Added fucntion prototype for plant.step

--- simulator/plant/__init__.py
+++ simulator/plant/__init__.py
@@ -1,2 +1,9 @@
-def propagate(envstate, rocketstate):
-    return RocketState
+def step(rockstate, envstate, dt):
+    """
+    :param rockstate: the current state off the rocket (type RocketState)
+    :param envstate: the current state of the environment (type EnvironmentState)
+    :param dt: the amount of time to step forward in seconds
+    :return: The predicted state of the rocket after time dt with appropriate
+        effects of the environment and motor applied to it
+    """
+    raise NotImplementedError
