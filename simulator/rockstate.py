478b865d631dec016a42a36d3e5514f074ae716b
Varun Iyer <varun_iyer@posteo.net>
2020-05-31 14:59:59 -0700

    Remove unnecessary getters; scipy spatial Rotation

--- simulator/rockstate.py
+++ simulator/rockstate.py
@@ -1,4 +1,5 @@
 import numpy as np
+from scipy.spatial.transform import Rotation
 
 class RocketState(np.ndarray):
     """
@@ -18,86 +19,7 @@ class RocketState(np.ndarray):
     LENGTH = .51 # length of the rocket
     position = np.array(0) # position of base
     velocity = np.array(0) # position of base
-    avelocity = np.array(0) # position of base
-    orientation = 0 # store this in euler angles or quaternions
+    angular_position = Rotation() # scipy spatial Rotation of angular position
+    angular_velocity = np.array(0) # 3-vector of angular velocity
     abrake_extension = np.array(0) # 3-vector for each airbrake extension in radians
     abrake_amps_prop = np.array(0) # 3-vector for the current through each abrake motor
-
-    def euler(self):
-        """
-        :returns: ndarray(double) 3-vector of rocket roll, pitch, yaw
-        """
-        raise NotImplementedError
-        return roll, pitch, yaw
-
-    def quaternion(self):
-        """
-        :returns: ndarray(quat) 1-vector of quaternion pose
-        """
-        raise NotImplementedError
-        return quat
-
-    def pos_com(self):
-        """
-        :returns: a numpy 3-vector of position at the center of mass
-        """
-        raise NotImplementedError
-        return xyz
-
-    def pos_top(self):
-        """
-        :return: ndarray 3-vector of position at the tip of the nose
-        """
-        raise NotImplementedError
-        return xyz
-
-    def pos_abrake(self):
-        """
-        :return: ndarray 3-vector of position position at 
-            the center of the tube at the top of the airbrakes
-        """
-        raise NotImplementedError
-        return xyz
-
-    def mass(self):
-        """
-        :return: current mass of rocket as a float
-        """
-        raise NotImplementedError
-        return kg
-
-    def velocity(self):
-        """
-        :return: current velocity of rocket as ndarray(double) 3-vector
-        """
-        raise NotImplementedError
-        return v
-
-    def euler_velocity(self):
-        """
-        :return: ndarray(double) 3-vector of angular velocity
-            roll, pitch, yaw over time
-        """
-        raise NotImplementedError
-        return omega
-
-    def quaternion_velocity(self):
-        """
-        :return: ndarray(quaternion) 1-vector of angular pose over time
-        """
-        raise NotImplementedError
-        return omega
-
-    def abrake(self):
-        """
-        :return: ndarray(double) 3-vector of the extension of each airbrake
-        """
-        raise NotImplementedError
-        return self.abrake_extension
-
-    def abrake_amps(self):
-        """
-        :return: ndarray(double) 3-vector of current through each airbrake motor
-        """
-        raise NotImplementedError
-        return self.abrake_amps_prop
