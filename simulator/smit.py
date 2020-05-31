79c27d7ba7314da5178e9d6983b958c9f3075352
Varun Iyer <varun_iyer@posteo.net>
2020-05-30 21:46:39 -0700

    Accidentally switched `next` and `__iter__`

--- simulator/smit.py
+++ simulator/smit.py
@@ -16,19 +16,14 @@ class SimIter:
     dt = 0 # small timestep
 
     def __iter__(self):
-        # do controls
-        # propagate airbrakes
-        # propagate environment
-        
-        self.rockstate = plant.step(self.rockstate, self.envstate, self.dt)
-        abrake_amps = controller.sysinput(self.rockstate)
-        self.rockstate = abrakes.step(self.rockstate)
-        return self.rockstate
+        return self
 
     def next(self):
         """
         :return: the state of the rocket at time t + dt
         side effects: advances time to t + dt
         """
-        raise StopIteration
-        return
+        self.rockstate = plant.step(self.rockstate, self.envstate, self.dt)
+        abrake_amps = controller.sysinput(self.rockstate)
+        self.rockstate = abrakes.step(self.rockstate)
+        return self.rockstate
