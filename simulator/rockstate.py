c683707afee94f1f30f0d243b88229576afcc236
LBabati <lbabati@ucsb.edu>
2020-06-02 20:01:43 -0700

    For max

--- simulator/rockstate.py
+++ simulator/rockstate.py
@@ -1,6 +1,6 @@
 import numpy as np
 from scipy.spatial.transform import Rotation
-from .math import math
+import math
 
 class RocketState(np.ndarray):
     """
@@ -16,21 +16,19 @@ class RocketState(np.ndarray):
     
     All others are derived from these.
     """
-    MASS = 3  # fill in actual, initial mass of rocket
+    MASS = 3  # FIXME: , initial mass of rocket
     LENGTH = .51  # length of the rocket
     CM = 0.3  # fill in actual, initial center of mass from bottom of rocket
     position = np.array([0, 0, CM])  # position of CM
     momentum = np.zeros(3)  # initial momentum of the rocket
-    velocity = momentum / MASS
     q = np.array([0, 0, 0, 1])  # initial orientation of the rocket as quaternion
     orientation = Rotation.from_quat(q)  # Rotation object that stores the orientation
     angular_momentum = np.zeros(3)  # initial angular momentum of the rocket
-    angular_velocity = np.zeros(3)  # 3-vector of angular velocity
     abrake_extension = np.array(0)  # 3-vector for each airbrake extension in radians
     abrake_amps_prop = np.array(0)  # 3-vector for the current through each abrake motor
 
-
     def get_velo(self, momentum):
+        return momentum / self.MASS
 
     def quat2mat(self, r):
         """
@@ -40,4 +38,50 @@ class RocketState(np.ndarray):
         r = r.as_matrix(r)
         return r
 
+    def rot_inert(self): # FIXME: needs to change as mass changes
+        ixx = 60  # FIXME: not correct values
+        iyy = 60
+        izz = 60
+        return np.array([ixx, 0, 0],
+                        [0, iyy, 0],
+                        [0, 0, izz])
 
+    def omega(self, rot_matrix, L):
+        """
+        :param rot_matrix: ndarray(double) the rotation matrix
+        :param L: ndarray(double) angular momentum
+        :return: the angular velocity of the rocket
+        """
+        return NotImplementedError
+
+    def v_app(self, v_omega, v_cm):
+        """
+        :param v_omega: ndarray(double) apparent velocity due to angular velocity
+        :param v_cm: ndarray(double) apparent velocity of the center of mass
+        :return: ndarray(double) the total apparent velocity
+        """
+        return NotImplementedError
+
+    def v_cm(self, wind, velocity):
+        """
+        :param wind: ndarray(double) vector for wind velocity
+        :param velocity: ndarray(double) velocity of rocket without wind
+        :return: ndarray(double) apparent velocity due to wind
+        """
+        return NotImplementedError
+
+    def v_omega(self, omega, roll_a, X_bar):
+        """
+        :param omega: ndarray(double) angular velocity vector
+        :param roll_a: roll axis
+        :param X_bar: distance between CM and CP
+        :return: ndarray(double) apparent velocity due to angular velocity
+        """
+        return NotImplementedError
+
+    def ang_of_att(self, v_app):
+        """
+        :param v_app: apparent velocity of the rocket
+        :return: angle of attack of the rocket
+        """
+        return NotImplementedError
