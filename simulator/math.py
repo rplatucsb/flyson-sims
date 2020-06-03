783e9d97a373fa85861c9df81b6af9b0e5113b6a
Varun Iyer <varun_iyer@posteo.net>
2020-06-02 18:45:05 -0700

    Made math not a class

--- simulator/math.py
+++ simulator/math.py
@@ -1,30 +1,30 @@
+"""
+Store the mathematical functions necessary for the calculations involved in the
+simulation
+"""
+
 import numpy as np
 from .envstate import EnvironmentState as es
 
-class math():
+def orien_in_ref(self, rot_matrix):
     """
-    Store the mathematical functions necessary for the calculations involved in the
-    simulation
+    :param rot_matrix: ndarray(double) that describes the rotation in matrix form
+    :return: ndarray(double) the axes of the rocket in the reference frame
     """
-    def orien_in_ref(self, rot_matrix):
-        """
-        :param rot_matrix: ndarray(double) that describes the rotation in matrix form
-        :return: ndarray(double) the axes of the rocket in the reference frame
-        """
-        yaw = np.dot(rot_matrix, es.ref_yaw)
-        pitch = np.dot(rot_matrix, es.ref_pitch)
-        roll = np.dot(rot_matrix, es.ref_roll)
-        return np.array([yaw, roll, pitch])
+    yaw = np.dot(rot_matrix, es.ref_yaw)
+    pitch = np.dot(rot_matrix, es.ref_pitch)
+    roll = np.dot(rot_matrix, es.ref_roll)
+    return np.array([yaw, roll, pitch])
 
 
-    def quat_dot(self, velocity, angular_velocity):  # may be better to put in smit, idk rn
-        """
-        :param velocity: ndarray(double) contains the current velocity vector
-        :param angular_velocity: ndarray(double) contains current angular velocity
-        :return: ndarray(double) derivative of the quaternion describing orientation
-        """
-        s_dot = 0.5 * (np.vdot(velocity, angular_velocity))
-        v_dot = 0.5 * (np.add(self.q[3] * angular_velocity,
-                       np.cross(velocity, angular_velocity)))
-        q_dot = [v_dot, s_dot]
-        return q_dot
+def quat_dot(self, velocity, angular_velocity):  # may be better to put in smit, idk rn
+    """
+    :param velocity: ndarray(double) contains the current velocity vector
+    :param angular_velocity: ndarray(double) contains current angular velocity
+    :return: ndarray(double) derivative of the quaternion describing orientation
+    """
+    s_dot = 0.5 * (np.vdot(velocity, angular_velocity))
+    v_dot = 0.5 * (np.add(self.q[3] * angular_velocity,
+                   np.cross(velocity, angular_velocity)))
+    q_dot = [v_dot, s_dot]
+    return q_dot
